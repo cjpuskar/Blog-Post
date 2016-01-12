6bb3a517e3543873d88a4613903d46e913e590db
Chris Puskar <cjpuskar@comcast.net>
2016-01-11 22:07:18 -0800

    [feat]: refactor server.js file

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
+// MODULES
 var express = require('express');
-var app = express();
 var path = require('path');
 
-var port = 8000;
+var app = express();
 
-// Viewed at http://localhost:8000
+// CONFIGURATION
+var port = process.env.PORT || 8000;
+
+// MIDDLEWARE
+app.use(express.static(__dirname + '/client'));
+
+// Routes
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
+	res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
+// START THE APP
 app.listen(port, function() {
   console.log("Simple blog post server listening on port " + port + "!");
 });
